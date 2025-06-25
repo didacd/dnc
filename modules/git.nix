a56be87624db555e22bb62dca285d5d14406cc43
didacdomenech <didac@didacdomenech.com>
2025-06-25 20:37:25 +0200

    minor changes - added a few comments to address most common use cases

--- modules/git.nix
+++ modules/git.nix
@@ -6,5 +6,9 @@ _: {
     # - vim-fugitive
     # - git-conflict
     # - gitlinker-nvim
+
+    # If you need support for Gitlab, I recommend using a TUI like:
+    # - https://github.com/NiklasTreml/gitlab-tui
+    # For GitHub use: denops-gh.vim
   };
 }
