89922b26012280edc3fed07804060a5edbf3be53
didacdomenech <didac@didacdomenech.com>
2025-10-01 22:46:04 +0200

    minor QoL improvements
    1. Copilot is now disabled by default
    2. OSC52 is enabled by default
    3. Minor keybinding adjustments
    4. Minor nvim-cmp adjustments

--- modules/copilot.nix
+++ modules/copilot.nix
@@ -1,6 +1,11 @@
 _: {
   config.vim.assistant.copilot = {
-    enable = true;
-    cmp.enable = true;
+    enable = false;
+    cmp.enable = false;
+    mappings.panel = {
+      accept = "<C-CR>"; # By default Ctrl + Enter should be better to avoid AI crap noise.
+      #jumpNext = "<C-]>"; # QoL between suggestions
+      #jumpPrev = "<C-[>";
+    };
   };
 }
